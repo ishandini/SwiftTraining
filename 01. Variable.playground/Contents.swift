0bac26174c770299d3797145e400f1284a31e599
Ishan Weerasooriya <Ishan@Emedias-MacBook-Air.local>
2017-05-22 16:41:08 +0530

    new code

--- 01. Variable.playground/Contents.swift
+++ 01. Variable.playground/Contents.swift
@@ -2,6 +2,93 @@
 
 import UIKit
 
-var str = "Hello, playground"
-var myInteger = 10
+var myStringArray: [String] = ["One", "Two", "Three"]
+myStringArray.append("Four")
+myStringArray.remove(at: 1)
+
+var myIntArray = [1,2,3,4]
+
+var newAnyArry:[Any] = [myStringArray, myIntArray]
+
+print(myStringArray)
+print(myIntArray)
+
+
+newAnyArry[0]
+newAnyArry[1]
+
+
+
+var intArray = newAnyArry[0] as! [String]
+
+if var newIntArray = newAnyArry[0] as? [String] {
+    newIntArray.remove(at: 0)
+    print(newIntArray)
+}
+
+
+class Student {
+
+    var firstName = ""
+    var age  = 0
+    var dob: NSDate?
+    var lastname = ""
+    var fullName: String {
+        return firstName + " " + lastname
+    }
+    
+    var score: Int = 10 {
+        willSet(myValue) {
+             print("Score WILLSET:\(myValue)")
+        }
+        
+        didSet {
+            print("Score DIDSET:\(oldValue)")
+        }
+    }
+    
+    
+    init(firstName: String, lastname: String, age: Int) {
+    
+        self.firstName = firstName
+        self.lastname = lastname
+        self.age = age
+        self.dob = NSDate()
+    }
+}
+
+
+var saman = Student(firstName: "Saman", lastname: "Perera", age: 20)
+saman.firstName = "Gayan"
+print(saman.firstName)
+print(saman.dob)
+print(saman.fullName)
+
+saman.score = 40
+saman.score = 90
+
+
+struct Dog {
+    var name = ""
+    var age = 10
+    var dob: Date?
+    
+}
+
+var lazzy = Dog(name: "Lazzy", age: 3, dob: nil)
+print(lazzy.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
